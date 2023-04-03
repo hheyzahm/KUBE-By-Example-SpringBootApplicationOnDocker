5214f0df1b1382556099c98cbed470dba78810eb
Hazeem Hassan <hhazeemhassan@gmail.com>
2023-04-03 09:42:24 +0500

    Initial commit

---
+++ src/main/java/kbe/spingbootrest/KubeByExampleSpringBootApplicationOnDockerApplication.java
@@ -0,0 +1,13 @@
+package kbe.spingbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeByExampleSpringBootApplicationOnDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeByExampleSpringBootApplicationOnDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kbe/spingbootrest/KubeByExampleSpringBootApplicationOnDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package kbe.spingbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeByExampleSpringBootApplicationOnDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
